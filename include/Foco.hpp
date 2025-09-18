5e812769533293db9a249f638a56d3a54f2a183a
Gil124231 <a24310362@live.ceti.mx>
2025-09-18 01:55:06 +0000

    practica 1

---
+++ include/Foco.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <EstadoFoco.hpp>
+
+class Foco
+{
+private:
+   EstadoFoco estadoActual;
+public:
+    Foco() {}
+    ~Foco() {}
+    void Encender(){}
+    void Apagar(){}
+    EstadoFoco MostrarEstado(){}
+};
